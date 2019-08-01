d6ed5ddb7257e6195cd7f0dc38016756fba0962f
Salvatore Ventrone <ventrosky@gmail.com>
2019-08-01 20:22:30 +0200

    URL Shortener Microservice
    API Project: added index page

--- api-urlshortener/services/web-server.js
+++ api-urlshortener/services/web-server.js
@@ -3,18 +3,27 @@ const express = require('express');
 const webServerConfig = require('../config/web-server.js');
 const morgan = require('morgan');
 const router = require('./router.js');
+const path=require("path")
+const cors = require('cors');
 
 let httpServer;
  
 function initialize() {
   return new Promise((resolve, reject) => {
     const app = express();
+
     httpServer = http.createServer(app);
+
+    app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+    app.use(express.static('public'));
     
     app.use(morgan('combined'));
-    //app.use(checkurls);
+
     app.use('/api', router);//http://server:port/api/shorturl/:url
 
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname + '/../views/index.html'));
+    });
  
     httpServer.listen(webServerConfig.port)
       .on('listening', () => {
